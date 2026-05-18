ef25358fed517095961e5a13591a75155fc9d698
tduluard <theo.duluard@etudiant.univ-rennes.fr>
2026-05-18 11:56:31 +0200

    Configure custom domain portfolio.theoduluard.fr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theoduluard.github.io',
+  site: 'https://portfolio.theoduluard.fr',
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
